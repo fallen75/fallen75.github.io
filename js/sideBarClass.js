3985edd6f2574a33ed64e655e6f8f936a6f26885
Ale <egexjeffer75@gmail.com>
2026-07-23 19:47:55 -0500

    v 1.0.7 Responsive Design Calculadora Mobile pt4

--- js/sideBarClass.js
+++ js/sideBarClass.js
@@ -1,20 +1,20 @@
-const toggleBtn = document.querySelector('.menu-toggle--header');
-const sidebar = document.querySelector('.sidebar');
-const overlay = document.getElementById('sidebarOverlay');
-const $mainContent = document.querySelector('.main-content');
-const $containerGlobal = document.querySelector('.container-global');
+// const toggleBtn = document.querySelector('.menu-toggle--header');
+// const sidebar = document.querySelector('.sidebar');
+// const overlay = document.getElementById('sidebarOverlay');
+// const $mainContent = document.querySelector('.main-content');
+// const $containerGlobal = document.querySelector('.container-global');
 
-function toggleMenu() {
-    //sidebar.classList.toggle('active');
-    //overlay.classList.toggle('active');
-    //$mainContent.classList.toggle('no-scroll');
-    //$containerGlobal.classList.toggle('no-scroll');
-    document.documentElement.classList.toggle('no-scroll');
-    document.body.classList.toggle('no-scroll');
-}
+// function toggleMenu() {
+//     //sidebar.classList.toggle('active');
+//     //overlay.classList.toggle('active');
+//     //$mainContent.classList.toggle('no-scroll');
+//     //$containerGlobal.classList.toggle('no-scroll');
+//     document.documentElement.classList.toggle('no-scroll');
+//     document.body.classList.toggle('no-scroll');
+// }
 
-// Abrir/Cerrar con el botón
-toggleBtn.addEventListener('click', toggleMenu);
+// // Abrir/Cerrar con el botón
+// toggleBtn.addEventListener('click', toggleMenu);
 
-// Cerrar si el usuario hace clic en el fondo oscuro
-overlay.addEventListener('click', toggleMenu);
+// // Cerrar si el usuario hace clic en el fondo oscuro
+// overlay.addEventListener('click', toggleMenu);
